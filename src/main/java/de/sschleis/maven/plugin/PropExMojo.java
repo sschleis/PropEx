facbe2e3bbbede80cd16df0b8d7886c0c281cdc5
sschleis <sebastian.schleis@1und1.de>
2016-05-27 15:03:15 +0200

    Einlesen von Resource Dir + Multi Value in einer Zeile

--- src/main/java/de/sschleis/maven/plugin/PropExMojo.java
+++ src/main/java/de/sschleis/maven/plugin/PropExMojo.java
@@ -17,7 +17,8 @@ import java.util.Scanner;
  * @goal propex
  * @phase install
  */
-public class PropExMojo extends AbstractMojo {
+public class PropExMojo extends AbstractMojo
+{
 
     /**
      * Project's source directory as specified in the POM.
@@ -27,10 +28,27 @@ public class PropExMojo extends AbstractMojo {
      * @required
      */
     private final File sourceDirectory = new File("");
+
+
     /**
-     * List with all files to be counted
+     * Project's resources directory as specified in the POM.
+     *
+     * @parameter expression="${resources.main.dir}"
+     * @readonly
+     * @required
      */
-    private final List<String> files = new LinkedList<String>();
+    private final File resourcesDirectory = new File("");
+
+    /**
+     * List with all srource Files to be counted
+     */
+    private final List<String> sourceFiles = new LinkedList<String>();
+
+    /**
+     * List with all resource Files to be counted
+     */
+    private final List<String> resourceFiles = new LinkedList<String>();
+
     /**
      * Location of the file.
      *
@@ -41,74 +59,154 @@ public class PropExMojo extends AbstractMojo {
     private List<SpringPropertie> properties = new LinkedList<SpringPropertie>();
     private String encoding = "UTF-8";
 
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    public void execute() throws MojoExecutionException, MojoFailureException
+    {
 
         File f = outputDirectory;
-        if (!f.exists()) {
+        if (!f.exists())
+        {
             f.mkdirs();
         }
 
-        if (!sourceDirectory.exists()) {
+        if (!sourceDirectory.exists())
+        {
             getLog().error("Source directory \"" + sourceDirectory + "\" is not valid.");
             return;
         }
 
-        fillListWithAllFilesRecursiveTask(sourceDirectory, files);
+        fillListWithAllFilesRecursiveTask(sourceDirectory, sourceFiles);
+        findProperties(sourceFiles);
 
-
-        findProperties();
+        if (!resourcesDirectory.exists())
+        {
+            getLog().warn("Resource directory \"" + resourcesDirectory + "\" is not valid.");
+        }
+        else
+        {
+            fillListWithAllFilesRecursiveTask(resourcesDirectory, resourceFiles);
+            findProperties(resourceFiles);
+        }
 
         File touch = new File(f, "propEx.txt");
 
         FileWriter w = null;
-        try {
+        try
+        {
             w = new FileWriter(touch);
             w.write("Properties found: " + properties.size() + "\n");
-            for (SpringPropertie propertie : properties) {
+            for (SpringPropertie propertie : properties)
+            {
                 w.append(propertie.toCsv());
             }
             w.flush();
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             throw new MojoExecutionException("Error creating file " + touch, e);
-        } finally {
-            if (w != null) {
-                try {
+        }
+        finally
+        {
+            if (w != null)
+            {
+                try
+                {
                     w.close();
-                } catch (IOException e) {
+                }
+                catch (IOException e)
+                {
                     // ignore
                 }
             }
         }
     }
 
-    private void findProperties() {
-        for (String file : files) {
-            try {
+    private void findProperties(final List<String> files)
+    {
+        for (String file : files)
+        {
+            try
+            {
                 final Scanner scan = new Scanner(new File(file), encoding);
-                String line = scan.nextLine();
-                while (scan.hasNext()) {
-                    if (line.contains("@Value")) {
-                        final SpringPropertie springPropertie = new SpringPropertie(line);
-                        properties.add(springPropertie);
+                String line = scan.nextLine().trim();
+                while (scan.hasNext())
+                {
+                    try
+                    {
+                        if (line.contains("${"))
+                        {
+                            cutProperties(line);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        getLog().warn(line, e);
                     }
                     line = scan.nextLine();
 
                 }
-            } catch (final IOException e) {
+            }
+            catch (final IOException e)
+            {
                 getLog().error(e.getMessage());
             }
         }
     }
 
-    private void fillListWithAllFilesRecursiveTask(final File root, final List<String> files) {
-        if (root.isFile()) {
+    private void cutProperties(final String line)
+    {
+        final int start = line.indexOf("${");
+        final int end = line.indexOf("}");
+        final String substring = line.substring(start + 2, end);
+        if (substring.contains(":"))
+        {
+            final int dp = substring.indexOf(":");
+            String name = substring.substring(0, dp);
+            String defaultValue = substring.substring(dp + 1, substring.length());
+            if (checkPropertie(name))
+            {
+                properties.add(new SpringPropertie(name, null, defaultValue, null));
+            }
+        }
+        else
+        {
+            if (checkPropertie(substring))
+            {
+                properties.add(new SpringPropertie(substring, null, null, null));
+            }
+        }
+
+        final String endLine = line.substring(end + 1, line.length());
+        if (endLine.contains("${"))
+        {
+            cutProperties(endLine);
+        }
+    }
+
+    private boolean checkPropertie(final String prop){
+        boolean result = true;
+        if(prop.contains("_"))
+            result = false;
+        if(prop.matches("[A-Z]*"))
+            result = false;
+
+        return result;
+    }
+
+    private void fillListWithAllFilesRecursiveTask(final File root, final List<String> files)
+    {
+        if (root.isFile())
+        {
             files.add(root.getPath());
             return;
         }
-        for (final File file : root.listFiles()) {
-            if (file.isDirectory()) {
+        for (final File file : root.listFiles())
+        {
+            if (file.isDirectory())
+            {
                 fillListWithAllFilesRecursiveTask(file, files);
-            } else {
+            }
+            else
+            {
                 files.add(file.getPath());
             }
         }
