9f08f7fdb63c96a52cd139f16f2077de553ab630
sschleis <sebastian.schleis@gmail.com>
2016-06-13 23:51:34 +0200

    Adding ADOC Writer

--- src/main/java/de/sschleis/maven/plugin/PropExMojo.java
+++ src/main/java/de/sschleis/maven/plugin/PropExMojo.java
@@ -7,11 +7,7 @@ import org.apache.maven.plugin.MojoFailureException;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Goal which touches a timestamp file.
@@ -19,8 +15,7 @@ import java.util.Scanner;
  * @goal propex
  * @phase install
  */
-public class PropExMojo extends AbstractMojo
-{
+public class PropExMojo extends AbstractMojo {
 
     /**
      * Project's source directory as specified in the POM.
@@ -58,20 +53,32 @@ public class PropExMojo extends AbstractMojo
      * @required
      */
     private File outputDirectory;
+
+    /**
+     * @parameter expression="${project.artifactId}
+     * @required
+     */
+    private String artifactId;
+
+    /**
+     * @parameter expression="${project.version}"
+     * @requiered
+     */
+    private String version;
+
     private Map<String, SpringPropertie> properties = new HashMap<String, SpringPropertie>();
     private String encoding = "UTF-8";
 
-    public void execute() throws MojoExecutionException, MojoFailureException
-    {
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        getLog().info(artifactId);
 
         File f = outputDirectory;
-        if (!f.exists())
-        {
+        if (!f.exists()) {
             f.mkdirs();
         }
 
-        if (!sourceDirectory.exists())
-        {
+        if (!sourceDirectory.exists()) {
             getLog().error("Source directory \"" + sourceDirectory + "\" is not valid.");
             return;
         }
@@ -79,134 +86,128 @@ public class PropExMojo extends AbstractMojo
         fillListWithAllFilesRecursiveTask(sourceDirectory, sourceFiles);
         findProperties(sourceFiles);
 
-        if (!resourcesDirectory.exists())
-        {
+        if (!resourcesDirectory.exists()) {
             getLog().warn("Resource directory \"" + resourcesDirectory + "\" is not valid.");
-        }
-        else
-        {
+        } else {
             fillListWithAllFilesRecursiveTask(resourcesDirectory, resourceFiles);
             findProperties(resourceFiles);
         }
 
-        File touch = new File(f, "propEx.txt");
+        String fileNameTXT = artifactId + "-" + version + "-" + "propEx.txt";
+        String fileNameADOC = artifactId + "-" + version + "-" + "propEx.adoc";
 
-        FileWriter w = null;
-        try
-        {
-            w = new FileWriter(touch);
-            w.write("Properties found: " + properties.size() + "\n");
-            for (SpringPropertie propertie : properties.values())
-            {
-                w.append(propertie.toCsv());
+        File touchTXT = new File(f, fileNameTXT);
+        File touchADOC = new File(f, fileNameADOC);
+
+        FileWriter writerTXT = null;
+        FileWriter writerADOC = null;
+        try {
+            writerTXT = new FileWriter(touchTXT);
+            writerTXT.write("Properties found: " + properties.size() + "\n");
+            for (SpringPropertie propertie : properties.values()) {
+                writerTXT.append(propertie.toCsv());
             }
-            w.flush();
-        }
-        catch (IOException e)
-        {
-            throw new MojoExecutionException("Error creating file " + touch, e);
-        }
-        finally
-        {
-            if (w != null)
-            {
-                try
-                {
-                    w.close();
-                }
-                catch (IOException e)
-                {
+
+            writerADOC = new FileWriter(touchADOC);
+            adocHeader(writerADOC);
+            adocBody(writerADOC, properties.values());
+            adocFooter(writerADOC);
+
+            writerTXT.flush();
+            writerADOC.flush();
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating file " + touchTXT, e);
+        } finally {
+            if (writerTXT != null) {
+                try {
+                    writerTXT.close();
+                } catch (IOException e) {
                     // ignore
                 }
             }
         }
     }
 
-    private void findProperties(final List<String> files)
-    {
-        for (String file : files)
-        {
-            try
-            {
+    private void adocHeader(FileWriter w) throws IOException {
+        w.write("[width=\"60%\",frame=\"topbot\",options=\"header\"]\n");
+        w.write("|======================\n");
+        w.write("|Propertie |Default\n");
+    }
+
+    private void adocFooter(FileWriter w) throws IOException {
+        w.write("|======================\n");
+    }
+
+    private void adocBody(FileWriter w, Collection<SpringPropertie> list) throws IOException {
+        for(SpringPropertie propertie : list){
+            String defaultValue = propertie.getDefaultValue() != null ? propertie.getDefaultValue() : "";
+            w.write("|" + propertie.getName() + " |" + defaultValue + "\n");
+        }
+    }
+
+
+    private void findProperties(final List<String> files) {
+        for (String file : files) {
+            try {
                 final Scanner scan = new Scanner(new File(file), encoding);
                 String line = scan.nextLine().trim();
-                while (scan.hasNext())
-                {
-                    try
-                    {
-                        if (line.contains("${"))
-                        {
+                while (scan.hasNext()) {
+                    try {
+                        if (line.contains("${")) {
                             cutProperties(line);
                         }
-                    }
-                    catch (Exception e)
-                    {
+                    } catch (Exception e) {
                         getLog().warn(line, e);
                     }
                     line = scan.nextLine();
 
                 }
-            }
-            catch (final IOException e)
-            {
+            } catch (final IOException e) {
                 getLog().error(e.getMessage());
             }
         }
     }
 
-    private void cutProperties(final String line)
-    {
+    private void cutProperties(final String line) {
         final int start = line.indexOf("${");
         final int end = line.indexOf("}");
         final String substring = line.substring(start + 2, end);
-        if (substring.contains(":"))
-        {
+        if (substring.contains(":")) {
             final int dp = substring.indexOf(":");
             String name = substring.substring(0, dp);
             String defaultValue = substring.substring(dp + 1, substring.length());
-            if (checkPropertie(name))
-            {
+            if (checkPropertie(name)) {
                 properties.put(name, new SpringPropertie(name, null, defaultValue, null));
             }
-        }
-        else
-        {
-            if (checkPropertie(substring))
-            {
+        } else {
+            if (checkPropertie(substring)) {
                 properties.put(substring, new SpringPropertie(substring, null, null, null));
             }
         }
 
         final String endLine = line.substring(end + 1, line.length());
-        if (endLine.contains("${"))
-        {
+        if (endLine.contains("${")) {
             cutProperties(endLine);
         }
     }
 
-    private boolean checkPropertie(final String prop){
+    private boolean checkPropertie(final String prop) {
         boolean result = true;
-        if(prop.matches("[A-Z_]*"))
+        if (prop.matches("[A-Z_]*"))
             result = false;
 
         return result;
     }
 
-    private void fillListWithAllFilesRecursiveTask(final File root, final List<String> files)
-    {
-        if (root.isFile())
-        {
+    private void fillListWithAllFilesRecursiveTask(final File root, final List<String> files) {
+        if (root.isFile()) {
             files.add(root.getPath());
             return;
         }
-        for (final File file : root.listFiles())
-        {
-            if (file.isDirectory())
-            {
+        for (final File file : root.listFiles()) {
+            if (file.isDirectory()) {
                 fillListWithAllFilesRecursiveTask(file, files);
-            }
-            else
-            {
+            } else {
                 files.add(file.getPath());
             }
         }
